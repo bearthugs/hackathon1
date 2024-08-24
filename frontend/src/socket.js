9884afd40f1ed95d90083233af07a032e97d24f4
shua0326 <shuyanghua123@gmail.com>
2024-08-25 01:12:08 +1000

    fixed sockets yay

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,15 @@
-// import { io } from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://127.0.0.1:5000';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://127.0.0.1:5000';
 
-// export const socket = io(URL);
+export const socket = io(URL, {
+    transports: ['websocket'], // Ensure WebSocket transport is used
+    autoConnect: false,
+    cors: {
+        origin: "*",
+    },
+});
+
+socket.on('connect_error', (err) => {
+    console.error('Connection error:', err);
+});
