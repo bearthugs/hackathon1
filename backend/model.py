422d7ee37c0fbdb252c78f8f23f71f8fc73f15ec
Clare Tian <claretian1405@gmail.com>
2024-08-24 18:34:55 +1000

    commit to pull

--- backend/model.py
+++ backend/model.py
@@ -6,11 +6,29 @@ class User:
         self.session_id = session_id #PK
         self.score = 0
     
+    def get_username(self):
+        return self.username
+    
+    def set_username(self, username):
+        self.username = username
+
+    def get_token(self):
+        return self.token
+    
+    def get_top_songs(self):
+        return self.top_songs
+    
+    def get_session_id(self):
+        return self.session_id
+
     def get_score(self):
         return self.score
     
     def increment_score(self):
         self.score += 1
+
+    def reset_score(self):
+        self.score = 0
 
 class Room:
     def __init__(self, id, time, questions, players, difficulty):
@@ -20,6 +38,39 @@ class Room:
         self.time = time
         self.questions = questions
         self.players = players
+
+    def get_id(self):
+        return self.id
+
+    def get_users(self):
+        return self.users
+    
+    def add_user(self, user):
+        self.users.append(user)
+        self.players += 1
+
+    def rmv_user(self, session_id):
+        for user in self.users:
+            if user.get_session_id == session_id:
+                self.users.remove(user)
+            break
+        self.players -= 1
+
+    def get_difficulty(self):
+        return self.difficulty
+    
+    def get_time(self):
+        return self.time
+    
+    def set_time(self, time):
+        self.time = time
+    
+    def get_questions(self):
+        return self.questions
+    
+    def set_questions(self, questions):
+        self.questions = questions
+
     
 
 
