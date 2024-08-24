248b74f75683566ea87b5a2d97b3317dce5635c1
clxyrz <clairefan27@gmail.com>
2024-08-24 16:27:29 +1000

    initial frontend setup

--- frontend/src/components/Toggle.jsx
+++ frontend/src/components/Toggle.jsx
@@ -2,7 +2,6 @@ import { Switch, FormControlLabel } from '@mui/material'
 import * as React from 'react'
 
 export function Toggle({ darkMode, handleThemeChange }) {
-  console.log(darkMode, handleThemeChange)
   return (
 <Switch checked={darkMode} onChange={handleThemeChange}/>
   )
--- frontend/src/page/Authentication.jsx
+++ frontend/src/page/Authentication.jsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
 import Button from '@mui/material/Button';
+import { getAuthentication } from '../function'
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const handleClick = (nav) => {
+    const response = getAuthentication()
+    console.log(response)
+}
+
 export const Authentication = () => {
+    const nav = useNavigate()
     return (
         <>
-
-                    <Button variant='Link' sx={{ backgroundColor: "green" }}>Click Me</Button>
+            <Button variant='Link' sx={{ backgroundColor: "green" }} onClick={() => handleClick()}>Click Me</Button>
         </>
     )
 }
