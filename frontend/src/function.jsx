4d250e297e4c55d9185c8094b52a83edc843f82f
clxyrz <clairefan27@gmail.com>
2024-08-25 04:09:54 +1000

    frontend

--- frontend/src/function.jsx
+++ frontend/src/function.jsx
@@ -1,20 +1,25 @@
 import axios from 'axios';
-
+import Cookies from 'js-cookie'
 export async function getAuthentication() {
     let response;
-    response = await axios.post('http://127.0.0.1:5000/authentication', { message: 'get authentication' })
+    const sessionid = Cookies.get('session');
+    response = await axios.post('http://127.0.0.1:5000/authentication', { message: 'get authentication', sid: sessionid })
     return response
 }
 
 export async function checkRoomId(code) {
     let response;
-    response = await axios.post('http://127.0.0.1:5000/join', { room_id: code })
+    const sessionid = Cookies.get('session');
+
+    response = await axios.post('http://127.0.0.1:5000/join', { room_id: code, sid: sessionid })
     return response
 }
 
 export async function giveInfo(player, time, song, difficulty) {
     let response;
-    response = await axios.post('http://127.0.0.1:5000/create_room', { players: player, time: time, songs: song, difficulty: difficulty })
+    const sessionid = Cookies.get('session');
+
+    response = await axios.post('http://127.0.0.1:5000/create_room', { players: player, time: time, songs: song, difficulty: difficulty, sid: sessionid })
     return response
 }
 
