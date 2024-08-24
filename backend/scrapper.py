cb410de6fbee1aed34907bf27643c6c53a19410d
Yutong Xu <yuxu0537@uni.sydney.edu.au>
2024-08-24 22:21:09 +1000

    merge

--- backend/scrapper.py
+++ backend/scrapper.py
@@ -79,7 +79,7 @@ def get_user_info():
     artist_track_tuples = [(artist['name'], track['name']) for track in tracks for artist in track['artists']]
     for artist, track in artist_track_tuples:
         top_tracks_names.append((artist, track))
-    return profile_name, top_tracks_names
+    return profile_name, top_tracks_names, access_token
 
 def format_artist(name):
     name = re.sub(r'[^a-z0-9 ]', '', name.lower()).strip()
